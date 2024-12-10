056c1f453f41794e89c7c4900e185e323e73bd85
Eduardo Fraga <eduardofraga1994@gmail.com>
2024-12-10 15:28:26 -0300

    implementacao flyweight

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using DesignPatterns.src.GOF.Structural.Flyweight;
+var tickets = new List<LotteryTicket>();
+GC.Collect();
+long memoryBefore = GC.GetTotalMemory(true);
+
+foreach (string line in File.ReadLines("C:\\Users\\eduar\\source\\repos\\test\\ConsoleApp1\\bin\\Debug\\net8.0\\loteria_timespan.csv"))
+{
+	var parts = line.Split(",");
+	string? draw = parts[0];
+	string? date = parts[1];
+	string? b1 = parts[2];
+	string? b2 = parts[3];
+	string? b3 = parts[4];
+	string? b4 = parts[5];
+	string? b5 = parts[6];
+	string? b6 = parts[7];
+
+	var ticket = new LotteryTicket(FlyweightFactory.Get(draw, int.Parse(date)), b1, b2, b3, b4, b5, b6);
+	//var ticket = new LotteryTicket(new DrawFlyweight(draw, int.Parse(date)), b1, b2, b3, b4, b5, b6);
+	tickets.Add(ticket);
+}
+
+long memoryAfter = GC.GetTotalMemory(true);
+long memoryUsed1 = memoryAfter - memoryBefore;
+Console.WriteLine($"Memória usada pela primeira implementação: {memoryUsed1} bytes");
---
+++ DesignPatterns/src/GOF/Structural/Flyweight/Main.cs
@@ -0,0 +1,53 @@
+﻿namespace DesignPatterns.src.GOF.Structural.Flyweight
+{
+	public class LotteryTicket
+	{
+		public LotteryTicket(DrawFlyweight draw, string bet1, string bet2, string bet3, string bet4, string bet5, string bet6)
+		{
+			Draw = draw;
+			Bet1 = bet1;
+			Bet2 = bet2;
+			Bet3 = bet3;
+			Bet4 = bet4;
+			Bet5 = bet5;
+			Bet6 = bet6;
+		}
+
+		public DrawFlyweight Draw {  get; set; }
+        public string Bet1 { get; set; }
+        public string Bet2 { get; set; }
+        public string Bet3 { get; set; }
+        public string Bet4 { get; set; }
+        public string Bet5 { get; set; }
+        public string Bet6 { get; set; }
+    }
+
+	public class DrawFlyweight
+	{
+		public DrawFlyweight(string draw, int date)
+		{
+			Draw = draw;
+			Date = Date = new DateTime(2020, 1, 1).AddDays(date);
+		}
+
+		public string Draw { get; set; }
+		public DateTime Date { get; set; }
+	}
+
+	public class FlyweightFactory
+	{
+		public static Dictionary<string, DrawFlyweight> cache = new Dictionary<string, DrawFlyweight>();
+
+		public static DrawFlyweight Get(string draw, int date)
+		{
+			var index = $"{draw}:${date}";
+
+			if (!cache.ContainsKey(index))
+			{
+				cache.Add(index, new DrawFlyweight(draw, date));
+			}
+
+			return cache[index];
+		}
+	}
+}
